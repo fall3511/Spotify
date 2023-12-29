b6d6d424f1636add14a58db09cac8db8b24fffeb
Anfaal Afrose <anfaal.afrose@gmail.com>
2023-12-30 03:19:34 +0500

    Front-end Commit

---
+++ src/views/SearchView.vue
@@ -0,0 +1,56 @@
+<script setup>
+import CategorySelect from "../components/CategorySelect.vue";
+</script>
+
+<template>
+    <div class="p-8">
+        <button type="button" class="text-white text-2xl font-semibold hover:underline cursor-pointer">
+            Podcasts to make you smarter
+        </button>
+        <div class="py-1.5"></div>
+        <div class="grid xl:grid-cols-5 lg:grid-cols-4 md:grid-cols-3 sm:grid-cols-2 gap-6">
+            <CategorySelect category="Podcasts" image="https://picsum.photos/id/110/300/300"/>
+            <CategorySelect category="Audiobooks" image="https://picsum.photos/id/111/300/300"/>
+            <CategorySelect category="Made For You" image="https://picsum.photos/id/112/300/300"/>
+            <CategorySelect category="New Release" image="https://picsum.photos/id/113/300/300"/>
+            <CategorySelect category="New Release" image="https://picsum.photos/id/114/300/300"/>
+            <CategorySelect category="Podcasts" image="https://picsum.photos/id/115/300/300"/>
+            <CategorySelect category="Audiobooks" image="https://picsum.photos/id/116/300/300"/>
+            <CategorySelect category="Made For You" image="https://picsum.photos/id/117/300/300"/>
+            <CategorySelect category="New Release" image="https://picsum.photos/id/118/300/300"/>
+            <CategorySelect category="Pop Rock" image="https://picsum.photos/id/154/300/300"/>
+            <CategorySelect category="Podcasts" image="https://picsum.photos/id/120/300/300"/>
+            <CategorySelect category="Audiobooks" image="https://picsum.photos/id/121/300/300"/>
+            <CategorySelect category="Made For You" image="https://picsum.photos/id/122/300/300"/>
+            <CategorySelect category="New Release" image="https://picsum.photos/id/123/300/300"/>
+            <CategorySelect category="Jazz" image="https://picsum.photos/id/124/300/300"/>
+            <CategorySelect category="Podcasts" image="https://picsum.photos/id/125/300/300"/>
+            <CategorySelect category="Audiobooks" image="https://picsum.photos/id/126/300/300"/>
+            <CategorySelect category="Made For You" image="https://picsum.photos/id/127/300/300"/>
+            <CategorySelect category="New Release" image="https://picsum.photos/id/128/300/300"/>
+            <CategorySelect category="New Release" image="https://picsum.photos/id/129/300/300"/>
+            <CategorySelect category="Podcasts" image="https://picsum.photos/id/130/300/300"/>
+            <CategorySelect category="Audiobooks" image="https://picsum.photos/id/145/300/300"/>
+            <CategorySelect category="Made For You" image="https://picsum.photos/id/176/300/300"/>
+            <CategorySelect category="New Release" image="https://picsum.photos/id/156/300/300"/>
+            <CategorySelect category="New Release" image="https://picsum.photos/id/225/300/300"/>
+            <CategorySelect category="Podcasts" image="https://picsum.photos/id/166/300/300"/>
+            <CategorySelect category="Audiobooks" image="https://picsum.photos/id/244/300/300"/>
+            <CategorySelect category="Made For You" image="https://picsum.photos/id/202/300/300"/>
+            <CategorySelect category="New Release" image="https://picsum.photos/id/199/300/300"/>
+            <CategorySelect category="Hard Metal" image="https://picsum.photos/id/155/300/300"/>
+            <CategorySelect category="Podcasts" image="https://picsum.photos/id/188/300/300"/>
+            <CategorySelect category="Audiobooks" image="https://picsum.photos/id/299/300/300"/>
+            <CategorySelect category="Made For You" image="https://picsum.photos/id/276/300/300"/>
+            <CategorySelect category="New Release" image="https://picsum.photos/id/256/300/300"/>
+            <CategorySelect category="New Release" image="https://picsum.photos/id/225/300/300"/>
+            <CategorySelect category="Podcasts" image="https://picsum.photos/id/183/300/300"/>
+            <CategorySelect category="Audiobooks" image="https://picsum.photos/id/222/300/300"/>
+            <CategorySelect category="Made For You" image="https://picsum.photos/id/171/300/300"/>
+            <CategorySelect category="New Release" image="https://picsum.photos/id/181/300/300"/>
+            <CategorySelect category="Pop" image="https://picsum.photos/id/189/300/300"/>
+            <CategorySelect category="J-Pop" iamge="https://picsum.photos/id/181/300/300"/>
+            <CategorySelect category="Indie" image="https://picsum.photos/id/171/300/300"/>
+        </div>
+      </div>
+</template>
